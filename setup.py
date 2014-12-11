d4237b727e655eeef9a6a832afa569213a433ccf
Jorge Herrera <jorgeh@ccrma.stanford.edu>
2014-12-11 00:01:14 -0800

    adding examples and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: jorgeh
+# @Date:   2014-12-10 23:59:17
+# @Last Modified by:   jorgeh
+# @Last Modified time: 2014-12-11 00:00:08
+
+from setuptools import setup
+
+setup(name='pygrfnn',
+      version='0.1',
+      description='Grdient Frequency Neural Networks',
+      url='jorge@ccrma-stanford.edu:/user/j/jorgeh/git/pygrfnn.git',
+      author='Jorge Herrera',
+      author_email='jorgeh@ccrma.stanford.edu',
+      license='MIT',
+      packages=['pygrfnn'],
+      include_package_data=True,
+      # setup_requires=...,
+      # dependency_links=...,
+      zip_safe=False
+      )
